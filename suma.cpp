6ae9bcd9683e5a70b60ee072175c25711325f3f5
juliannGarcia <gamj080214hgtrrla0@cbtis217.edu.mx>
2024-10-16 08:38:01 -0600

    Le quite comentarios

--- suma.cpp
+++ suma.cpp
@@ -1,14 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main (){
-    ///le agrego mis comentarios
-    ///declaro 3 variables para sumarlas
+
     int a,b,c;
 
-    ///leo las variables
     cin>>a>>b>>c;
 
-    ///imprimo
     cout<<a+b+c;
     return 0;
 }
